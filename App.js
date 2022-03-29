a7c21fb6c7396c8a1a63e895814256e1e940228c
learnreact21 <102497779+learnreact21@users.noreply.github.com>
2022-03-29 00:09:43 -0700

    updated customer data

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import customers from "./data/customers.json"; 
+import customers from "./data/customers.json";
 import Customer from './components/Customer';
 
 function App() {
   return (
     <div className="container-fluid m-5">
-     <h1>Customer Loyality Program</h1>
-    <Customer data={customers} /> 
+      <h1>Customer Loyality Program</h1>
+      <Customer data={customers} />
     </div>
   );
 }
