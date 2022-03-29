60c3b68be53f0e8bf680bca7cb0bd01487f98415
learnreact21 <102497779+learnreact21@users.noreply.github.com>
2022-03-28 23:11:33 -0700

    deleted not required files
    and trying to display data in the card using bootstrap

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,14 +1,31 @@
-
+const cardWidth = {
+    width: "18rem"
+}
 export default function Customer({ data }) {
+const nowDate =  new Date();
+nowDate.setMonth(nowDate.getMonth() - 3);
+console.log(nowDate.toLocaleDateString());
     return (
         <>
-            {data.length > 0 ?(
-                <>
-                there is more data
-                </>
+            {data.length > 0 ? (
+                <ol className="text-primary">
+                    {data.map((ele) => {
+                        return <div className="card p-3 m-2" style={cardWidth}>
+                            <h5 className="card-title">{ele.firstName},{ele.lastName} </h5>
+                            <div className="card-body">
+                                <p>Reward Point(s) Earned: {ele.rewardPoints}</p>
+                                {/* <p>Total Ampunt Spent: {ele.transactions.purchase.reduce(
+  (previousValue, currentValue) => previousValue + currentValue,
+  0
+)}
+</p> */}
+                            </div>
+                        </div>
+                    })}
+                </ol>
             ) : (
-            <>No results found!!</>
-    )}
+                <>No results found!!</>
+            )}
         </>
     )
 }; 
